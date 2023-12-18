3f120ea93ba3024bfc0d6f21b27d899fafb7c4ef
Zainal Abidin <zainal.abi@gmail.com>
2023-12-18 15:01:04 +0700

    modify run.systemd.sh because of renaming monitoring.py to Monitoring_Present_Weather.py

--- run.systemd.sh
+++ run.systemd.sh
@@ -1,1 +1,1 @@
-/home/meteopublik/miniconda3/envs/streamlit/bin/streamlit run /sites/monitoring-presentwx/monitoring.py --server.port=18501 --server.address=0.0.0.0
+/home/meteopublik/miniconda3/envs/streamlit/bin/streamlit run /sites/monitoring-presentwx/Monitoring_Present_Weather.py --server.port=18501 --server.address=0.0.0.0
